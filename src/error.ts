4bbdba1948df36021cb512c0fbac7fd6080c1f71
Tz-Huan Huang <tzhuan@potentia.asia>
2025-03-14 18:02:41 +0800

    Bump release 1.0.0

---
+++ src/error.ts
@@ -0,0 +1,25 @@
+export * from '@potentia/model/error'
+
+export class LockingError extends Error {
+  constructor(message?: string) {
+    super(message ?? 'Unknown Lock Error')
+  }
+}
+
+export class LockError extends LockingError {
+  constructor(message?: string) {
+    super(message ?? 'Lock Error')
+  }
+}
+
+export class RelockError extends LockingError {
+  constructor(message?: string) {
+    super(message ?? 'Relock Error')
+  }
+}
+
+export class UnlockError extends LockingError {
+  constructor(message?: string) {
+    super(message ?? 'Unlock Error')
+  }
+}
